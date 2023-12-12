cc2ffbdbd42a52593143ff9163d625195b6e3706
prosper williams <wprosper92@gmail.com>
2023-12-12 13:15:19 +0100

    still building

--- src/Pages/Main/Home.jsx
+++ src/Pages/Main/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 
    return (
       <div style={{backgroundColor: theme ? "#2222" : "#F7F7F8", color: theme=== 'dark' ? "#F7F7F8" : "#2222"}}>
-         <div className="border ml-[2.4rem] mt-1 sm:ml-0 flex justify-between items-start">
+         <div className=" ml-[2.4rem] mt-1 sm:ml-0 flex justify-between items-start">
             <h1 className="font-sans font-bold text-[#A303A0] text-md sm:text-lg">Home</h1>
             <div className="w-[2.5rem] h-[2.5rem] object-cover">
                <img src={CurrentDp} alt="" className="w-full h-full object-cover rounded-full" />
