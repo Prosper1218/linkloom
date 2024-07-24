38e05c972c405582c4f445842bfe14430d9a9e5d
prosper williams <wprosper92@gmail.com>
2024-07-24 14:59:09 +0100

    editing......

--- src/Pages/Main/Home.jsx
+++ src/Pages/Main/Home.jsx
@@ -5,22 +5,17 @@ useContext;
 
 const Home = () => {
    const {CurrentDp} = useContext(DpContext);
-   const {theme} = UseTheme()
+   const {theme} = UseTheme();
    //    {Math.random().toString().slice(3,7)}
 
    return (
-      <div style={{backgroundColor: theme ? "#2222" : "#F7F7F8", color: theme=== 'dark' ? "#F7F7F8" : "#2222"}}>
+      <div className="min-h-[90vh]" style={{backgroundColor: theme ? "#2222" : "#F7F7F8", color: theme === "dark" ? "#F7F7F8" : "#2222"}}>
          <div className=" ml-[2.4rem] mt-1 sm:ml-0 flex justify-between items-start">
             <h1 className="font-sans font-bold text-[#A303A0] text-md sm:text-lg">Home</h1>
             <div className="w-[2.5rem] h-[2.5rem] object-cover ">
                <img src={CurrentDp} alt="" className="w-full h-full object-cover rounded-full" />
             </div>
          </div>
-
-
-
-
-
 
          <p className="text-5xl text-white">please navigate to the profile page !!</p>
       </div>
